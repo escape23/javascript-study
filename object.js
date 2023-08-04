e01589c6ba5e75f2c42f2cb715ca8e666d6ed827
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2023-08-04 12:43:55 +0200

    object

--- object.js
+++ object.js
@@ -15,19 +15,72 @@
 // const array = [1, 2, 3, 4, 5];
 // console.log(typeof (array));
 
-const book = {
-  title: "The Last Kingdom",
-  author: "Bernard Cornwell",
-  genres: ["historical prose", "adventure"],
-  isPublic: true,
-  rating: 8.38,
-};
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   isPublic: true,
+//   rating: 8.38,
+// };
 
-const bookTitle = book.title;
-alert(bookTitle); // 'The Last Kingdom'
+// const bookTitle = book.title;
+// alert(bookTitle); // 'The Last Kingdom'
 
-const bookGenres = book.genres;
-alert(bookGenres); // ['historical prose', 'adventurs']
+// const bookGenres = book.genres;
+// alert(bookGenres); // ['historical prose', 'adventurs']
 
-const bookPrice = book.price;
-alert(bookPrice); // undefined
+// const bookPrice = book.price;
+// alert(bookPrice); // undefined
+
+// function sum (numbers) {
+//     "use strict";
+//   let total;
+//     for(let i = 0; i < numbers.length; i += 1){
+//       total += numbers[i];
+// }
+//     return total;
+// };
+
+// const user = {
+//   name: "Jacques Gluke",
+//   tag: "jgluke",
+//   location: {
+//     country: "Jamaica",
+//     city: "Ocho Rios",
+//   },
+//   hobbies: ["swiming", "music", "sci-fi"],
+// };
+
+// const location = user.location;
+// console.log(location); // Об'єкт location
+
+// const country = user.location.country;
+// console.log(country); // 'Jamaica'
+
+
+// const propName = "name";
+// const user = {
+//   age: 25,
+//   // Ім'я цієї властивості буде взяте зі значення змінної propName
+//   name: "Генрі Сибола",
+// };
+
+// console.log(user.name); // 'Генрі Сибола'
+
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// alert(keys)
+// console.log(keys)
+
+// const values = Object.values(book);
+// alert(values);
+// console.log(values);
+
+// const keys = Object.keys(book);
+// console.log(keys); // ['title', 'author', 'genres', 'rating']
