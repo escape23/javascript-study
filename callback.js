98f0073bf00061dfd78d2918e0584f70876558e1
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2023-08-10 11:46:34 +0200

    +

---
+++ callback.js
@@ -0,0 +1,20 @@
+// function repeatLog(n) {
+//   for (let i = 0; i < n; i += 1) {
+//     console.log(i);
+//   }
+// }
+
+// repeatLog(5);
+
+
+const numbers = [5, 10, 15, 20, 25];
+
+// Класичний for
+// for (let i = 0; i < numbers.length; i += 1) {
+//   console.log(`Індекс ${i}, значення ${numbers[i]}`);
+// }
+
+// Метод перебирання forEach
+numbers.forEach(function (number, index) {
+  console.log(`Індекс ${index}, значення ${number}`);
+});
