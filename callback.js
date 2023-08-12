743a9de9e90bd49c97a3292c390b234e29dcf8f4
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2023-08-12 20:33:49 +0200

    1

--- callback.js
+++ callback.js
@@ -7,7 +7,7 @@
 // repeatLog(5);
 
 
-const numbers = [5, 10, 15, 20, 25];
+// const numbers = [5, 10, 15, 20, 25];
 
 // Класичний for
 // for (let i = 0; i < numbers.length; i += 1) {
@@ -15,6 +15,85 @@ const numbers = [5, 10, 15, 20, 25];
 // }
 
 // Метод перебирання forEach
-numbers.forEach(function (number, index) {
-  console.log(`Індекс ${index}, значення ${number}`);
-});
+// numbers.forEach(function (number, index) {
+//   console.log(`Індекс ${index}, значення ${number}`);
+// }); 
+
+//     const fnA = function (message, callback) {
+//     console.log(message)
+
+//     console.log(callback)
+//     callback(100);
+// }
+
+// const fnB = function (number) {
+//     console.log('Log', number)
+// }
+// fnA('jfgngjn', fnB)
+
+
+// const doMath = function (a, b, callback) {
+//     const result = callback(a, b);
+
+//     console.log(result);
+// }
+
+
+// doMath(2, 3, function (x, y) {
+//     return x + y;
+// } )
+// doMath(2, 3, function (x, y) {
+//     return x * y;
+// } )
+
+
+// const filter = function (array, test) {
+//     const filteredArray = [];
+
+//     for (const el of array) {
+//         console.log(el);
+//         console.log(test(el));
+//     }
+//     return filteredArray;
+// }
+
+
+// const callback1 = function (value) {
+//     return value >= 3;
+// };
+
+// const r1 = filter([1, 2, 3, 4, 5], callback1);
+// console.log(r1);
+
+
+// const fnA = function (parameter) {
+//     const innerVariable = 'value of fnA'
+//     const innerFunction = function () {
+//         console.log(innerVariable);
+//         console.log('Its calling innerFunction');
+//     };
+//     return innerFunction;
+// }
+
+// const fnB = fnA();
+
+// fnB();
+
+
+// console.log(fnB);
+
+
+const add = function (a, b, c) {
+    console.log(a, b, c);
+    return a + b + c;
+}
+
+const addArro2 = (a, b, c) => {
+    console.log(a, b, c);
+    return a + b + c;
+}
+
+const addArrow = (a, b, c) =>  a + b + c;
+
+
+console.log(add(5, 10, 15));
