bd70438bd9f8cf4aa8020574696d82c9ae803413
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2023-08-16 16:35:55 +0200

    this

--- methods-arrays.js
+++ methods-arrays.js
@@ -78,26 +78,26 @@
  *    - если коллбек вернул false элемент НЕ добавляется в возвращаемый массив
  */
 
-const numbers = [5, 10, 15, 20, 25];
+// const numbers = [5, 10, 15, 20, 25];
 
-const filteredNumbers = numbers.filter(number =>  number < 10 || number > 20)
+// const filteredNumbers = numbers.filter(number =>  number < 10 || number > 20)
 // console.log(filteredNumbers)
 
-const players = [
-  { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
-  { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
-  { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
-  { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
-  { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
-];
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
+// ];
 
-const onlinePlayers = players.filter(player => player.online)
+// const onlinePlayers = players.filter(player => player.online)
 // console.log(onlinePlayers)
 
-const offlinePlayers = players.filter(player => !player.online)
+// const offlinePlayers = players.filter(player => !player.online)
 
 
-const hardPlayers = players.filter(player => player.timePlayed > 250)
+// const hardPlayers = players.filter(player => player.timePlayed > 250)
 // console.log(hardPlayers)
 
 /*
@@ -125,3 +125,77 @@ const hardPlayers = players.filter(player => player.timePlayed > 250)
 // const uniqueGenres = allGenres.filter(
 //   (genre, index, array) => array.indexOf(genre) === index
 // );
+
+// const getFriends = (users) => {
+//    const moreFriends = users.flatMap(user => user.friends)
+// const uniqueFriends = moreFriends.filter(
+//    (user, index, array) => array.indexOf(user) === index)
+// return uniqueFriends;
+// }
+// Change code above this line
+
+
+/*
+ * Array.prototype.find()
+ * - Поэлементо перебирает оригинальный массив
+ * - Возвращает первый элемент удовлетворяющий условию или undefined
+ */
+
+// const numbers = [5, 10, 15, 20, 25];
+
+// const number = numbers.find(number => number > 5)
+// console.log(number)
+
+// const players = [
+//   { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: false },
+//   { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: true },
+//   { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+//   { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+//   { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
+// ];
+
+// const playerIdToFind = 'player-3';
+// const playerId = players.find(player => player.id === playerIdToFind);
+
+// console.log(playerId)
+
+
+/*
+ * Array.prototype.every()
+ * - Поэлементо перебирает оригинальный массив
+ * - Возвращает true если все элементы массива удовлетворяют условию
+ */
+
+const players = [
+  { id: 'player-1', name: 'Mango', timePlayed: 310, points: 54, online: true },
+  { id: 'player-2', name: 'Poly', timePlayed: 470, points: 92, online: false },
+  { id: 'player-3', name: 'Kiwi', timePlayed: 230, points: 48, online: true },
+  { id: 'player-4', name: 'Ajax', timePlayed: 150, points: 71, online: false },
+  { id: 'player-5', name: 'Chelsy', timePlayed: 280, points: 48, online: true },
+];
+
+
+const isAllOnline = players.every(player => player.online);
+// console.log(isAllOnline)
+
+
+/*
+ * Array.prototype.some()
+ * - Поэлементо перебирает оригинальный массив
+ * - Возвращает true если хотя бы один элемент массива удовлетворяет условию
+ */
+const isAnyOnline = players.some(player => player.online);
+// console.log('isAnyOnline', isAnyOnline)
+
+const anyHardcorePlayers = players.some(player => player.timePlayed > 300);
+// console.log(anyHardcorePlayers)
+
+
+/*
+ * Array.prototype.reduce()
+ * - Поэлементо перебирает оригинальный массив
+ * - Возвращает что угодно 🤯
+ * - Заменяет всё на свете, но использовать нужно с умом
+ */
+
+const numbers = [5, 10, 15, 20, 25];
