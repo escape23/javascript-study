e3a83942b0190caf22a8bd50c751595781befade
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2023-09-07 18:17:04 +0200

    +

---
+++ js-event/02-forms.js
@@ -0,0 +1,35 @@
+/*
+ * - Событие submit
+ * - Действия браузера по умолчанию
+ * - Свойство elements
+ * - Класс FormData - https://developer.mozilla.org/en-US/docs/Web/API/FormData
+ */
+
+const form = document.querySelector('.js-register-form')
+
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+    event.preventDefault()
+
+    const formData = new FormData(event.currentTarget);
+
+    formData.forEach((value, name) => {
+    console.log('onFormSubmit -> name', name);
+    console.log('onFormSubmit -> value', value);
+});}
+
+// form.addEventListener('submit', onFormSubmit);
+
+// function onFormSubmit(event) {
+//   event.preventDefault();
+
+//   const formData = new FormData(event.currentTarget);
+
+//   console.log(formData);
+
+//   formData.forEach((value, name) => {
+//     console.log('onFormSubmit -> name', name);
+//     console.log('onFormSubmit -> value', value);
+//   });
+// }
